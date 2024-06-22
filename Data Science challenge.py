28f99456cb5eda95e254d08f121d219a78bf8be8
Firdavs <119061460+Firdavs222@users.noreply.github.com>
2024-06-23 00:38:07 +0500

    Update Data Science challenge.py

--- Data Science challenge.py
+++ Data Science challenge.py
@@ -5,18 +5,16 @@ Created on Sat May 25 19:40:11 2024
 @author: user
 """
 
-
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.losses import BinaryCrossentropy
+from xgboost import XGBClassifier
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import roc_auc_score, roc_curve
 
 
-train_df = pd.read_csv("D:/Downloads/Files/Files/train.csv")
-test_df = pd.read_csv("D:/Downloads/Files/Files/test.csv")
+train_df = pd.read_csv("train.csv")
+test_df = pd.read_csv("test.csv")
 
 X_train = train_df[["MonthlyCharges", "TotalCharges","ViewingHoursPerWeek",
                     "AverageViewingDuration","ContentDownloadsPerMonth","UserRating",
@@ -24,61 +22,18 @@ X_train = train_df[["MonthlyCharges", "TotalCharges","ViewingHoursPerWeek",
                     ]]
 Y_train = train_df["Churn"]
 
-model = Sequential([
-    Dense(units = 15, activation = 'sigmoid'),
-    Dense(units = 10, activation = 'sigmoid'),
-    Dense(units = 1, activation = 'linear')
-    ])
+model = XGBClassifier(n_estimators=100, max_depth=3, learning_rate=0.1)
 
-model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate = (1e-3)/2), 
-              loss = BinaryCrossentropy(from_logits = True))
-
-model.fit(X_train, Y_train, epochs = 20)
-
-logit = model(X_train)
-
-f_x = tf.nn.sigmoid(logit)
+model.fit(X_train, Y_train, early_stopping_rounds=10, eval_set=[(X_train, Y_train)], eval_metric='logloss', verbose=True)
 
 X_test = test_df[["MonthlyCharges", "TotalCharges","ViewingHoursPerWeek",
                     "AverageViewingDuration","ContentDownloadsPerMonth","UserRating",
                     "SupportTicketsPerMonth","WatchlistSize"
                     ]]
 
-logit2 = model(X_test)
-
-f2_x = tf.nn.sigmoid(logit2)
-
-Y_test = []
-for P in f2_x:
-    if P >= 0.5:
-        P = 1
-        Y_test.append(P)
-    else:
-        P = 0
-        Y_test.append(P)
-
+xgb_pred_prob = model.predict_proba(X_test)[:, 1]
 dict_df = {"CustomerID": test_df["CustomerID"],
         "predicted_probability": f2_x.tolist()
         }
 
-new_df = pd.DataFrame(dict_df)
-
-print(f"Model's AUC score: {roc_auc_score(Y_train.tolist(), f_x.tolist())}")
-
-
-def plot_roc_curve(Y_train, f_x):
-    """
-    plots the roc curve based of the probabilities
-    """
-
-    fpr, tpr, thresholds = roc_curve(Y_train, f_x)
-    plt.plot(fpr, tpr)
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-
-plot_roc_curve(Y_train, f_x)
-
-
-
-
-
+prediction_df = pd.DataFrame(dict_df)
